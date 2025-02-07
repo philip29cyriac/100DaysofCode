895c54fdd3b73520b17fa8ca44eaff3d6a5f0415
Philip Cyriac <148213154+philip29cyriac@users.noreply.github.com>
2025-02-07 15:18:43 +0000

    Day1 - BandNamegenerator
    - creating a simple python program that generates the band name for a person

---
+++ Day 1 Band Name Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator")
+
+city_name = input("What is the name of the city you grew up in? ")
+pet_name = input("What is the name of your pet? ")
+
+print("Your band name is",city_name + " " + pet_name)
---
+++ task.py
@@ -0,0 +1,3 @@
+username = input("What is your username?")
+length = len(username)
+print("The Length of the username is:", length)
