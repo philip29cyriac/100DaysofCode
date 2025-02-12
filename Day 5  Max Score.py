6f03baa24769b93d5dbf2b7dcc58f7bdc55194ef
Philip Cyriac <148213154+philip29cyriac@users.noreply.github.com>
2025-02-12 14:42:49 +0000

    Day 5

---
+++ Day 5  Max Score.py
@@ -0,0 +1,9 @@
+student_scores = [140,150,160,176,189,199,298]
+
+max_score = 0
+
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print(max_score)
---
+++ Day 5 - Range function.py
@@ -0,0 +1,6 @@
+gauss_value = 0
+
+for number in range(1,101):
+    gauss_value += number
+
+print(gauss_value)
